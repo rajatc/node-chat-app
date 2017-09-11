2eed4ecdb6ca5f94b133a2cc08dea40172d0153c
Rajat Choudhury <choudhuryrajat@gmail.com>
2017-09-11 09:42:51 +0530

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,28 @@
+var socket = io();
+
+socket.on('connect', function(){
+    console.log('Connected to server');
+
+    // socket.emit('createEmail', {
+    //     to: 'anjali@example.com',
+    //     text: 'Hey, this is Rajat'
+    // });
+
+    socket.emit('createMessage', {
+        from: 'Rajat',
+        text: 'My text message'
+    })
+});
+
+socket.on('disconnect', function(){
+    console.log('Disconnected from Server');
+});
+
+// socket.on('newEmail', function(email){
+//     console.log('New Email', email);
+// });
+
+socket.on('newMessage', function(message) {
+    console.log('New message' ,message);
+});
+
--- server/server.js
+++ server/server.js
@@ -14,9 +14,29 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    // socket.emit('newEmail', {
+    //     from: "rajat@example.com",
+    //     text: "Hey Wassup?",
+    //     createdAt: 123
+    // });
+
+    // socket.on('createEmail', (newEmail) => {
+    //     console.log('CreateEmail', newEmail);
+    // });
+
+    socket.emit('newMessage', {
+        from: 'Arun',
+        text: 'see you soon',
+        createdAt: 123123
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('Create message', message);
+    });
+
     socket.on('disconnect', () => {
         console.log('User was disconnected');
-    })
+    });
 })
 
 server.listen(port, () => {
