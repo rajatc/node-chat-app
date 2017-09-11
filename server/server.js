960babd4d4e134fa34c79e07553b8866e13504c4
Rajat Choudhury <choudhuryrajat@gmail.com>
2017-09-11 10:05:40 +0530

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -30,6 +30,18 @@ io.on('connection', (socket) => {
     //     createdAt: 123123
     // });
 
+    socket.emit('newMessage', {
+        from: "Admin",
+        text: "Welcome to the Chat App",
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: "Admin",
+        text: "New user joined",
+        createdAt: new Date().getTime()
+    })
+
     socket.on('createMessage', (message) => {
         console.log('Create message', message);
         io.emit('newMessage', {
@@ -37,6 +49,11 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
